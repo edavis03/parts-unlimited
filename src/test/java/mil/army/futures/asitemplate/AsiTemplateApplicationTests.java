fc7b063a7b896e0c9117d79e60a4dbb0d94a072c
Michael Edoror <michael.o.edoror.ctr@army.mil>
2022-01-07 15:08:29 -0600

    Initial Commit

---
+++ src/test/java/mil/army/futures/asitemplate/AsiTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package mil.army.futures.asitemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsiTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
