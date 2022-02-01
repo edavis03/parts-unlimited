3dfb765701c3b8b27fb1684d0b409114c00420fc
Michael Edoror <michael.o.edoror.ctr@army.mil>
2022-02-01 16:09:03 -0600

    Refactor frontend and cypress tests

--- frontend/src/__tests__/App.test.tsx
+++ frontend/src/__tests__/App.test.tsx
@@ -6,35 +6,37 @@ import {createTeam, getTeams} from "../teamsApiClient";
 
 jest.mock("../teamsApiClient");
 
-const getTeamsApiClient = getTeams as jest.MockedFunction<typeof getTeams>;
+const mockGetTeams = getTeams as jest.MockedFunction<typeof getTeams>;
+const mockCreateTeam = createTeam as jest.MockedFunction<typeof createTeam>;
+
+const addTeam = (teamName: string) => {
+  userEvent.type(screen.getByLabelText("Team Name"), teamName);
+  userEvent.click(screen.getByRole("button", {name: /submit/i}));
+}
 
 describe("Teams Page", () => {
   describe("when I view teams", () => {
     it("should display existing teams", async () => {
-      getTeamsApiClient.mockResolvedValue(["a team", "a second team"]);
+      mockGetTeams.mockResolvedValue(["a team", "a second team"]);
 
       render(<App/>);
 
-      const listItems = await screen.findAllByRole("listitem");
-      expect(listItems[0].innerHTML).toEqual("a team");
-      expect(listItems[1].innerHTML).toEqual("a second team");
+      expect(await screen.findByText("a team")).toBeInTheDocument();
+      expect(screen.getByText("a second team")).toBeInTheDocument();
     });
   });
 
   describe("when I add a new team", () => {
     it("should display the new team in existing teams", async () => {
-      when(createTeam)
-        .calledWith("a team")
-        .mockResolvedValueOnce("a team");
-
-      getTeamsApiClient.mockResolvedValueOnce([]);
-      getTeamsApiClient.mockResolvedValueOnce(["a team"]);
+      mockCreateTeam.mockResolvedValueOnce("a team");
+      mockGetTeams.mockResolvedValueOnce([]);
+      mockGetTeams.mockResolvedValueOnce(["a team"]);
 
       render(<App/>);
+      addTeam("a team");
 
-      userEvent.type(screen.getByLabelText("Team Name"), "a team");
-      userEvent.click(screen.getByRole("button", {name: /submit/i}));
-      expect(await screen.findByText("a team")).toBeVisible();
+      expect(mockCreateTeam).toHaveBeenCalledWith("a team");
+      expect(await screen.findByText("a team")).toBeInTheDocument();
     });
   });
 });
--- journey/cypress/integration/1-our-app/teams.spec.ts
+++ journey/cypress/integration/1-our-app/teams.spec.ts
@@ -3,8 +3,8 @@ const addTeam = (team: string) => {
   cy.findByRole("button").click();
 }
 describe("teams", () => {
-  describe("creating a team", () => {
-    it("updates the displayed list of teams", () => {
+  describe("when adding a team", () => {
+    it("should display the new team", () => {
       cy.visit("http://localhost:8080");
       addTeam("our-team-name");
       cy.findByText("our-team-name").should("exist");
