32dfa845ee1fb96dad3c02568292bfe1b50deff8
edavis03 <emily.davis.ctr@swf.army.mil>
2022-02-04 12:04:20 -0500

    Change team reference that slipped by

--- frontend/src/productsApiClient.ts
+++ frontend/src/productsApiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export async function createProduct(teamName: string): Promise<string> {
-  return (await axios.post<string>("/products", teamName, {headers: {'Content-Type': 'text/plain'}})).data
+export async function createProduct(product: string): Promise<string> {
+  return (await axios.post<string>("/products", product, {headers: {'Content-Type': 'text/plain'}})).data
 }
 
 export async function getProducts(): Promise<string[]> {
